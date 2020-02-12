714e91611fa5068a2e22e34d5e0c0bb0a06594a5
Uwaish Ali Khan <49112789+uwaishkhan55@users.noreply.github.com>
2020-02-12 10:19:36 +0530

    Update profile.js

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -5,6 +5,7 @@ class profile extends Component {
     constructor(props) {
         super(props)
         this.state={
+            image:null,
             location:"NEW DELHI",
             username:localStorage.getItem('profile'),
            i3:false,
@@ -17,6 +18,7 @@ class profile extends Component {
            console.log(this.state.userProfile)
 
           this.setState(  {
+              image:data.user.image,
               location:data.user.location,
               i3:data.user.interest.i3,
               i2:data.user.interest.i2,
@@ -30,7 +32,7 @@ class profile extends Component {
 
 
         if(localStorage.getItem('token')===null||localStorage.getItem('profile')===null){
-            window.location='/login'
+            window.location='/'
         }
    
     }
@@ -51,15 +53,16 @@ class profile extends Component {
         return (
            <div className="container" style={{textAlign:"center"}}>
            <p>Welcome {this.state.username}</p>
-        <h3>Your location is {this.state.location}</h3>
-           <h3>Your interests are:</h3>
-           {this.state.i1? <button style={{margin:"10px"}} type="button" class="btn btn-primary">
+      {(this.state.image)?<img src={this.state.image}  style={{ borderRadius:"20%" , width:"200px" , height:"250px"}} />:""}
+                          <h3>Your location is {this.state.location}</h3>
+                          <h3>Your interests are:</h3>
+                          {this.state.i1? <button style={{margin:"10px"}} type="button" class="btn btn-primary">
                           interest <span class="badge badge-light">1</span>
                           </button>:""}
                           {this.state.i2? <button style={{margin:"10px"}}type="button" class="btn btn-primary">
                           interest <span class="badge badge-light">2</span>
                           </button>:""}
-         {this.state.i3? <button type="button" style={{margin:"10px"}}class="btn btn-primary">
+                           {this.state.i3? <button type="button" style={{margin:"10px"}}class="btn btn-primary">
                           interest <span class="badge badge-light">3</span>
                           </button>:""}
                           {this.state.i4? <button style={{margin:"10px"}}type="button" class="btn btn-primary">
@@ -68,8 +71,9 @@ class profile extends Component {
                           {this.state.i5? <button style={{margin:"10px"}} type="button" class="btn btn-primary">
                           interest <span class="badge badge-light">5</span>
                           </button>:""}
-           
-           <button onClick={this.onClick} type="submit"  className="btn  btn-primary">LogOut</button>
+              
+              <button onClick={this.onClick} type="submit" style={{margin:"10px"}} className="btn  btn-primary">LogOut</button>
+             <button type="button" style={{margin:"10px"}} className="btn btn-success">Start Blogging</button>
            </div>
         )
     }
